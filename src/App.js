a20a19a6261d2d545afa537a4e4e1d1719958a16
Rohit <rsabi2708@gmail.com>
2023-01-02 19:57:02 +0530

    member-function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ function App() {
   let project = "Counter Application";
   let btnValue = "Click Me";
 
+  let clickMe = () => alert();
+
   return (
     <div>
       <h1>{project}</h1>
-      <input type="button" value={btnValue} />
+      <input type="button" value={btnValue} onClick={clickMe} />
     </div>
   );
 }
