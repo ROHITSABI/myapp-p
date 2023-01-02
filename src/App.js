3c25a17c69c56d98f29e55209349b0703c86e757
Rohit <rsabi2708@gmail.com>
2023-01-02 21:48:22 +0530

    map-list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 function App() {
   let title = "Map Demo";
-  let list = ["Mumbai", "Delhi"];
+  let [list, setList] = useState([]);
+
+  let addItemIntoList = () => {
+    let newList = [...list, "Navi Mumbai"];
+
+    setList(newList);
+  };
 
   return (
     <div>
       <h1>{title}</h1>
+      <input
+        type="button"
+        value="Add Item Into List"
+        onClick={addItemIntoList}
+      />
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
