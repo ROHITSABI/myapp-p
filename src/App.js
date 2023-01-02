2b68cb4d589a6f10053a332d32d25e0c518d81d7
Rohit <rsabi2708@gmail.com>
2023-01-02 20:04:23 +0530

    stateless-variable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 function App() {
-  let project = "Counter Application";
-  let btnValue = "Click Me";
-
-  let clickMe = () => alert();
+  let count = 100;
+  let increment = () => {
+    count = count + 1;
+    console.log(count);
+  };
 
   return (
     <div>
-      <h1>{project}</h1>
-      <input type="button" value={btnValue} onClick={clickMe} />
+      <h1>Counter Application</h1>
+      <h1>{count}</h1>
+      <input type="button" value="Increment" onClick={increment} />
     </div>
   );
 }
