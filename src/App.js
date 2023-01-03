504dd98c8916781d87c7d54ba3b68fd34d885a6f
Rohit <rsabi2708@gmail.com>
2023-01-03 19:02:40 +0530

    inline-internal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import { useState } from "react";
-
 function App() {
-  let title = "Map Demo";
-  let [list, setList] = useState([]);
-
-  let addItemIntoList = () => {
-    let textRef = document.querySelector("#text-id");
-    let newList = [textRef.value];
-
-    setList(newList);
-  };
-
+  let mystyle = { color: "white", background: "red" };
   return (
     <div>
-      <h1>{title}</h1>
-      <input type="text" id="text-id" />
-      <input
-        type="button"
-        value="Add Item Into List"
-        onClick={addItemIntoList}
-      />
-      {list.map((item) => (
-        <h1>{item}</h1>
-      ))}
+      <h1 style={mystyle}>Hello World</h1>
+      <h1 style={{ color: "whitesmoke", background: "red" }}>Hello</h1>
     </div>
   );
 }
