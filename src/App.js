e890247743821a78e0c23a99cf45ab51f5d85284
Rohit <rsabi2708@gmail.com>
2023-01-03 20:27:38 +0530

    dynamic-css-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import { useState } from "react";
 
 function App() {
+  let [list] = useState([1, 1, 1, 1]);
   let [theme, setTheme] = useState("Primary");
+
   let primaryTheme = () => {
     theme = "primary";
 
     setTheme(theme);
   };
 
+  let successTheme = () => {
+    theme = "success";
+
+    setTheme(theme);
+  };
+
+  let dangerTheme = () => {
+    theme = "danger";
+
+    setTheme(theme);
+  };
   return (
     <div>
-      <h1 className={`bg-${theme} text-dark p-3`}>BootStrap Styling</h1>
-      <input
-        className="btn btn-primary"
-        type="button"
-        value="Primary Theme"
-        onClick={primaryTheme}
-      />
+      <div className="sticky-top">
+        <h1 className={`bg-${theme} text-dark p-3`}>BootStrap Styling</h1>
+        <input
+          className="btn btn-primary"
+          type="button"
+          value="Primary Theme"
+          onClick={primaryTheme}
+        />
+        <input
+          className="btn btn-success"
+          type="button"
+          value="Success Theme"
+          onClick={successTheme}
+        />
+        <input
+          className="btn btn-danger"
+          type="button"
+          value="Danger Theme"
+          onClick={dangerTheme}
+        />
+      </div>
+
+      {list.map((item) => (
+        <div className={`alert alert-${theme} my-1`}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          unde dolore corporis enim nobis nostrum officiis ab obcaecati? Odit
+          dolorem soluta commodi, aperiam repellat dolores sunt numquam? Ut,
+          ipsa aspernatur!
+        </div>
+      ))}
     </div>
   );
 }
+
 export default App;
