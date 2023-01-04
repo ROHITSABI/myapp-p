6c7a9d7a3c1721467dc5516a5c219df7f6e4b5f2
Rohit <rsabi2708@gmail.com>
2023-01-04 21:58:25 +0530

    get-all-messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  let [list] = useState(["delhi", "mumbai", "Bhilai", "Pune"]);
+  let [title] = useState("API");
+  let [messageList, setMessageList] = useState([]);
+
+  let getAllMessages = async () => {
+    let url = "http://localhost/3001/mesages";
+    let response = await axios.get(url);
+    messageList = [...response.data];
+    setMessageList(messageList);
+  };
 
   return (
     <div>
-      <h1>Map Demo</h1>
-      {list.map((item) => (
-        <div className="alert alert-primary">{item}</div>
+      <h1>{title}</h1>
+      <input
+        type="button"
+        value="Make AJAX/API Call"
+        onClick={getAllMessages}
+      />
+      {messageList.map((item) => (
+        <div>{item.message}</div>
       ))}
     </div>
   );
